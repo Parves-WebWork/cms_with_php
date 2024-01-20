8a1374bc3f1fdeb6cbdb89120cd4706dd1230479
Prince-Mosharaf <princeparves79@gmail.com>
2024-01-21 02:53:31 +0600

    cms with php mysqli

---
+++ classes/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Config {
+
+
+    const SMTP_HOST = 'sandbox.smtp.mailtrap.io';
+
+
+    const SMTP_PORT = 2525;
+
+    const SMTP_USER = '6e9245ccd95bbf';
+
+    const SMTP_PASSWORD = 'f941af7c188112';
+
+
+
+}
